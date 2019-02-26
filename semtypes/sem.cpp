f483590d8a72fb232e46bf125a68ee4d50e22e38
xtofl <kristoffel.pirard@gmail.com>
2019-02-26 17:09:27 +0100

    add gsl

--- semtypes/sem.cpp
+++ semtypes/sem.cpp
@@ -3,10 +3,21 @@
 #include <numeric>
 #include <iterator>
 #include <algorithm>
+// https://www.gnu.org/software/gsl/doc/html/index.html
+#include "gsl/gsl_fit.h"
 
 int main() {
     std::vector<double> increments{{10, 5, 4, 5, 7, 10, 10, 7}};
     std::vector<double> timestamps{{1, 2, 3, 4, 5, 6, 7, 8}};
+
+    struct Fit {
+        double c0, c1;
+        double cov00, cov01, cov11;
+        double sumsq;
+    } fit;
+    const auto status = gsl_fit_linear(&timestamps[0], 1, &increments[0], 1, timestamps.size(),    
+        &fit.c0, &fit.c1, &fit.cov00, &fit.cov01, &fit.cov11, &fit.sumsq
+    );
 
     std::vector<double> delta_ts;
     std::adjacent_difference(begin(timestamps), end(timestamps), back_inserter(delta_ts));
